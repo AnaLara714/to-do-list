8b63332dd8f98ad72f2de121eea74ed583fbbf0f
AnaLara714 <Analara714@gmail.com>
2023-07-22 14:10:39 -0300

    1 - Gerenciamento de estado

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReact, useReducer } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Container, Header } from "native-base";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const initialState = {
+  count: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return initialState;
+    default:
+      return initialState;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [state, dispacth] = useReducer(reducer, initialState);
+
+  return (
+    <Container>
+      <Header>
+        <Text>Count: {state.count}</Text>
+      </Header>
+      <Button onPress={() => dispacth({ type: "decremente" })}>
+        <Text>Increment</Text>
+      </Button>
+    </Container>
+  );
+}
